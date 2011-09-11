98461bec7a662ddb220a563c725977a8104f3723
Marcos Wright Kuhns <webmaster@kuhnsfam.com>
2011-09-11 12:58:54 -0700

    Detect when we're nearing the bottom of the page.

--- infinite_stellario.js
+++ infinite_stellario.js
@@ -1,2 +1,13 @@
-console.log('hello setllar.io');
-console.log($('#pagination'));
+(function() {
+  var watchScroll = function() {
+    var pxUntilScroll = Math.max(window.innerHeight * 3, 500);
+    if($('#canvas').height() - window.pageYOffset > pxUntilScroll) {
+      getNext();
+    }
+  };
+  var getNext = function() {
+    var nextPage = $('#pagination a').first().attr('href');
+  };
+  
+  $(window).bind('scroll', watchScroll);
+})();
