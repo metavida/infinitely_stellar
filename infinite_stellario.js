66a460387e279ce72f149b929ed4022f0ad6b4eb
Marcos Wright Kuhns <webmaster@kuhnsfam.com>
2011-09-11 14:46:20 -0700

    Better in-code comments.

--- infinite_stellario.js
+++ infinite_stellario.js
@@ -10,32 +10,36 @@
     var nextPageEl = $('#pagination .paging a').first(),
       nextPageStatus = nextPageEl.data('infinite');
     
+    // Only try to paginate if there's a "Next >" button.
     if(nextPageEl.size() > 0) {
-      switch(nextPageStatus + '') {
-        case 'undefined':
-          $.ajax({
-            url: nextPageEl.attr('href'), 
-            dataType: 'html',
-            success: function(gottenHtml) {
-              try {
-                placeHolder.html(gottenHtml);
-                var nextContentEls = placeHolder.find('#main').children().
-                  not('#nav').not('#entity-nav-bk-lg').not('.notify-top');
-                if(nextContentEls.size() > 3) {
-                  $('#pagination').next().remove();
-                  $('#pagination').after(nextContentEls).remove();
-                  $('#main').find('.rule + .rule').remove();
-                } else {
-                  nextPageEl.data('infinite', 'done');
-                }
-              } catch(err) { nextPageEl.data('infinite', 'js-error: '+err.message); }
-            },
-            error: function(gottenHtml, err) {
-              nextPageEl.data('infinite', 'http-error: '+err);
-            }
-          });
-          nextPageEl.data('infinite', 'loading');
-          break;
+      // If there is no "infinite" stats data set on the "Next" button
+      // that means we haven't requested the next page worth of data yet.
+      if(typeof(nextPageStatus) == 'undefined') {
+        nextPageEl.data('infinite', 'loading');
+        $.ajax({
+          url: nextPageEl.attr('href'), 
+          dataType: 'html',
+          success: function(gottenHtml) {
+            try {
+              placeHolder.html(gottenHtml);
+              // Get the main content of the next page, but remove elements
+              // like the header, etc. that we don't want to repeat.
+              var nextContentEls = placeHolder.find('#main').children().
+                not('#nav').not('#entity-nav-bk-lg').not('.notify-top');
+              // Only append the content if the next page appears to not be empty.
+              if(nextContentEls.size() > 3) {
+                $('#pagination').next().remove();
+                $('#pagination').after(nextContentEls).remove();
+                $('#main').find('.rule + .rule').remove();
+              } else {
+                nextPageEl.data('infinite', 'done');
+              }
+            } catch(err) { nextPageEl.data('infinite', 'js-error: '+err.message); }
+          },
+          error: function(gottenHtml, err) {
+            nextPageEl.data('infinite', 'http-error: '+err);
+          }
+        });
       }
     }
   };
