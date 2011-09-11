3decf3180d89037cb9efdfd190281c6fced0b6ed
Marcos Wright Kuhns <webmaster@kuhnsfam.com>
2011-09-11 13:13:34 -0700

    Get the contents of the next page.

--- infinite_stellario.js
+++ infinite_stellario.js
@@ -1,12 +1,23 @@
 (function() {
   var watchScroll = function() {
     var pxUntilScroll = Math.max(window.innerHeight * 3, 500);
-    if($('#canvas').height() - window.pageYOffset > pxUntilScroll) {
+    if($('#canvas').height() - window.pageYOffset < pxUntilScroll) {
       getNext();
     }
   };
+  var placeHolder = $(document.createElement('div'));
   var getNext = function() {
-    var nextPage = $('#pagination a').first().attr('href');
+    var nextPageEl = $('#pagination a').first(),
+      nextPageStatus = nextPageEl.data('infinite');
+    
+    switch(nextPageStatus + '') {
+      case 'undefined':
+        placeHolder.load(nextPageEl.attr('href'), function() {
+          nextPageEl.data('infinite', 'success');
+        });
+        nextPageEl.data('infinite', 'loading');
+        break;
+    }
   };
   
   $(window).bind('scroll', watchScroll);
